a3cd5cdb7ae75a55733a4752a51ddd0ffae68479
JohanVillard <styx.locke@gmail.com>
2024-07-22 16:59:05 +0200

    Create struct for conditional search in _printf

--- _printf.c
+++ _printf.c
@@ -10,16 +10,37 @@
  */
 int _printf(const char *format, ...)
 {
+	va_list data;
 	unsigned int count = 0;			/* Number of characters */
-	int (*f)(char c);				
+	int i = 0;
+	parser_t modulo_parser[] = {	/*  */
+		{"c", print_char},
+		{NULL, NULL},
+	};
 
-	f = &print_char;
-
-		while (*format)				/* pass on the word */
+	while (*format)				/* Run the string */
 	{
-		count += (*f)(*format);		/* Count length of string */
+		if (*format == '%')
+		{
+			while (modulo_parser[i].specifier != NULL)
+			{
+				if (modulo_parser[i].specifier == *(format + 1))
+				{
+					modulo_parser[i].f(data);
+					format += 2;
+					if (*format != '%')
+					{
+						break;
+					}
+				}
+				i++;
+			}
+		}
+		i = 0;
+		count++;					/* Count length of string */
 		format++;					/* Move format index by 1 */
 	}
+	va_end(data);
 
 	print_char(count + '0');
 
--- main.h
+++ main.h
@@ -2,10 +2,24 @@
 #define MAIN_H
 
 #include <unistd.h>
+#include <stdarg.h>
+/**
+ * struct parser - Return function after format match
+ * @specifier: format to print
+ * @f: function associates to format
+ */
+typedef struct parser
+{
+	char *specifier;
+	int (*f)(va_list);
+} parser_t;
+
+/* ----------------------------------------------------- */
 
 int _printf(const char *format, ...);
-
 int print_char(char c);
+
+/* ----------------------------------------------------- */
 
 #endif /* MAIN.H */
 
