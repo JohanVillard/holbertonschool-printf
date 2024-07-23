f41c3bb06f5f3cf6bf5447592fd6a31d3fe266a3
JohanVillard <styx.locke@gmail.com>
2024-07-23 20:37:28 +0200

    Correct comment

--- print_hex.c
+++ print_hex.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_hex - Convert a decimal number
- * into hex number with uppercase and print
+ * into hex number with lowercase and print
  * @decimal_num: integer to convert and to print
  *
  * Return: The number of bytes of converted integer
@@ -22,14 +22,14 @@ int print_hex(unsigned int decimal_num)
 		if (temp < 10)
 			temp = temp + 48;			/* Add 48 refering to ASCII table */
 		else
-			temp = temp + 87;			/* Add 55 refering to ASCII table */
+			temp = temp + 87;			/* Add 87 refering to ASCII table */
 
 		hexa_Number[i] = temp;
 		i++;
 		decimal_num = decimal_num / 16;
 	}
 
-	for (j = i - 1; j > 0; j--)
+	for (j = i - 1; j > 0; j--)			/* Print in reverse */
 	{
 		_printf("%c", hexa_Number[j]);
 		count++;
