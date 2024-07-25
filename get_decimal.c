817257156441649ed0eaba4620358c0f042e989d
JohanVillard <styx.locke@gmail.com>
2024-07-25 17:20:55 +0200

    Add comment

--- get_decimal.c
+++ get_decimal.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
-* get_decimal - Get an integer from a va_list.
-* @data: A va_list containing the integer to conver
+* get_decimal - Get an integer from a va_list and return the printed count
+* @data: A va_list containing the integer to convert
 * in decimal and to be printed.
 *
-* This function extracts an integer from the provided va_list.
-* Then call print_int function that converts in decimal and print it.
+* Descrition: This function extracts an integer from the provided va_list.
+* then call print_int function and print it.
 *
-* Return: Return Number of bytes of number.
+* Return: Return mumber of bytes printed.
 */
 int get_decimal(va_list data)
 {
 	int count = 0;									/* Counter of bytes */
-	unsigned int num = va_arg(data, unsigned int);	/* Extract the next arg*/
+	unsigned int num = va_arg(data, unsigned int);	/* Extract the next data arg */
 
-	count += print_decimal(num);								/* Call the function that will print */
+	count += print_decimal(num);					/* Call the function to print */
 
 	return (count);									/* Return Number of bytes */
 }
