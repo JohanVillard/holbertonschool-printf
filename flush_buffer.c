82fa28f26bf229ed46428a24aca62a72bc8a4e76
JohanVillard <styx.locke@gmail.com>
2024-07-26 16:01:08 +0200

    Change for ||

--- flush_buffer.c
+++ flush_buffer.c
@@ -12,7 +12,7 @@ int flush_buffer(char *buffer, int *i_buffer)
 {
 	int count = 0;
 	/* Buffer is full or new line */
-	if (buffer[*i_buffer] == '\n' && *i_buffer == 1024)
+	if (buffer[*i_buffer] == '\n' || *i_buffer == 1024)
 	{
 		write(1, buffer, *i_buffer);
 		*i_buffer = 0;										/* Reset */
