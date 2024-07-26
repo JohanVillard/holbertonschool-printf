2200506a14e2f3feac177b8100feb24d5d3ce3e3
JohanVillard <styx.locke@gmail.com>
2024-07-26 18:47:36 +0200

    Delete hard coded region

--- print_address.c
+++ print_address.c
@@ -14,15 +14,7 @@ int print_address(va_list data, char *buffer, int *i_buffer)
 	buffer[*i_buffer] = '0';						/* Store the char */
 	count += flush_buffer(buffer, i_buffer);
 	buffer[*i_buffer] = 'x';						/* Store the char */
-	count += flush_buffer(buffer, i_buffer);
-	buffer[*i_buffer] = '7';						/* Store the char */
-	count += flush_buffer(buffer, i_buffer);
-	buffer[*i_buffer] = 'f';						/* Store the char */
-	count += flush_buffer(buffer, i_buffer);
-	buffer[*i_buffer] = 'f';						/* Store the char */
-	count += flush_buffer(buffer, i_buffer);
-	buffer[*i_buffer] = 'e';						/* Store the char */
-	count += flush_buffer(buffer, i_buffer);
+
 
 	count += get_hex(data, buffer, i_buffer);
 
