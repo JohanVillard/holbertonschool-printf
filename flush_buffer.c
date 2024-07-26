0a6a880e681a41aef9a6867202be333246b8bdac
JohanVillard <styx.locke@gmail.com>
2024-07-26 14:45:39 +0200

    Add flush buffer when carriage return

--- flush_buffer.c
+++ flush_buffer.c
@@ -10,7 +10,8 @@
  */
 void flush_buffer(char *buffer, int *i_buffer)
 {
-		if (*i_buffer == 1024)				/* Buffer s full */
+		if (*i_buffer == 1024 || buffer[*i_buffer] == '\n')				/* Buffer is full */
+																		/* or carriage return */
 		{
 			write(1, buffer, *i_buffer);
 			*i_buffer = 0;					/* Reset */
