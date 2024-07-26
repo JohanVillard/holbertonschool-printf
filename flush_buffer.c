87f5417c7e7a8d28c6fb743f2a0c5f2dba88050c
JohanVillard <styx.locke@gmail.com>
2024-07-26 16:22:54 +0200

    Free and re-malloc buffer

--- flush_buffer.c
+++ flush_buffer.c
@@ -11,10 +11,12 @@
 int flush_buffer(char *buffer, int *i_buffer)
 {
 	int count = 0;
-	/* Buffer is full or new line */
+	/* Buffer is full and new line */
 	if (buffer[*i_buffer] == '\n' && *i_buffer == 1024)
 	{
 		write(1, buffer, *i_buffer);
+		free(buffer);
+		buffer = malloc(1024);
 		*i_buffer = 0;										/* Reset */
 	}
 	else
