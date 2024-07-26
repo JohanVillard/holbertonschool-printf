19ea798701067c5e120cff94751931f80f7a857c
JohanVillard <styx.locke@gmail.com>
2024-07-26 15:50:05 +0200

    Flush buffer when equal to '\0'

--- flush_buffer.c
+++ flush_buffer.c
@@ -12,7 +12,8 @@ int flush_buffer(char *buffer, int *i_buffer)
 {
 	int count = 0;
 	/* Buffer is full or new line */
-	if (buffer[*i_buffer] == '\n' && *i_buffer == 1024)
+	if (buffer[*i_buffer] == '\n' && *i_buffer == 1024
+							&& buffer[*i_buffer] == '\0')
 	{
 		write(1, buffer, *i_buffer);
 		free(buffer);
